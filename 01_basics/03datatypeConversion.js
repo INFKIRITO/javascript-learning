e18169f6c409f549d75638a698ed88f4f5ca0d09
Aman Singh <94058048+INFKIRITO@users.noreply.github.com>
2024-03-12 07:39:48 +0000

    study today

---
+++ 01_basics/03datatypeConversion.js
@@ -0,0 +1,25 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN type is a number confusing
+//true=>1; false=> 0
+let isLoggedIn = "aman"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 =>true; 0=> false
+//"" => fals;
+//"aman" => true
+
+let someNumber = 33
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
