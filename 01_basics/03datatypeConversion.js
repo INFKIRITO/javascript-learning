f25b2277b6cb23db37354bfec84efa508ec38367
Aman Singh <94058048+INFKIRITO@users.noreply.github.com>
2024-03-12 07:58:39 +0000

    study again

--- 01_basics/03datatypeConversion.js
+++ 01_basics/03datatypeConversion.js
@@ -13,7 +13,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = "aman"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //1 =>true; 0=> false
 //"" => fals;
@@ -21,5 +21,39 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 let StringNumber = String(someNumber)
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+
+//***************operations *******************/
+
+let value = 3
+let negValue = -value
+// // console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " aman"
+
+let str3 = str1 + str2
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+console.log(+true); // really bad code
+console.log(+"");
+let num1, num2, num3
+num1 = num2 = num3 = 3 + 3
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
