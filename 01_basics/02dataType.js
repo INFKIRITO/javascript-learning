bd40f5ab33b3c435b8e879c1896a6934764c52d7
Aman Singh <94058048+INFKIRITO@users.noreply.github.com>
2024-03-12 07:22:34 +0000

    read this today

---
+++ 01_basics/02dataType.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3)// availabel but not applicable on nodejs its can work on browser
+
+//code should be redable and not like
+
+// console.log(3+3) ; console.log("aman")
+//primitive datatypes
+//number => 2 to power 53
+// bigint 
+// string=> ""
+// boolean => t/f
+// null=> standalone value
+// undefined => value has not been assigned
+// symbol => to find uniqueness 
+
+//object
+
+console.log(typeof null);
+//null is an object
+console.log(typeof undefined);//its a type of its own undefined
---
+++ 01_basics/o1variables.js
@@ -0,0 +1,11 @@
+const accountId = 12345
+let accountName = "aman Singh"
+var accountNo = "23432252"
+accountCity = "jaipur"
+
+/*
+prefer not to use variable 
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountName,accountNo,accountCity])
