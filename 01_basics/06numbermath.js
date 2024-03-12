f5bac523c7421c6d233ea1ea8a89e2f7f0d9ea24
Aman Singh <94058048+INFKIRITO@users.noreply.github.com>
2024-03-12 15:15:33 +0000

    lect on math

---
+++ 01_basics/06numbermath.js
@@ -0,0 +1,27 @@
+// const score = 400
+// console.log(score);
+ 
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 3523.8966
+// console.log(otherNumber.toPrecision(4));
+
+// ****************************************maths******************************
+console.log(Math);//
+// console.log(Math.abs(-4)); //only converts -neg to positive
+// console.log(Math.round(4.6)); // convert to the nearest number
+// console.log(Math.ceil(4.3)); // get the next highest 5
+// console.log(Math.floor(4.3)); // get the next lowerst 4
+// console.log(Math.max(4,5,7,8,1,9)); // get the next highest
+// console.log(Math.min(4,1,5,6,2)); // get the next highest
+console.log(Math.random()); // most used math method gives values btween 0 and 1
+console.log(Math.floor(Math.random()*10) + 1); //to get a value more than one and less than 9 
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * ( max - min + 1 )) + min );//generic syntax formula
