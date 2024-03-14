5a703d75a4bee22eb22969c0e6d0df7e41707125
Aman Singh <94058048+INFKIRITO@users.noreply.github.com>
2024-03-14 14:15:59 +0000

    function2

--- 03_basics/01Functions.js
+++ 03_basics/01Functions.js
@@ -34,4 +34,42 @@ function loginUserMessage(username){
 }
 // const message = loginUserMessage("aman")
 // const message = loginUserMessage("aman")
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+// ***********************function 2*************
+
+// function calculateCartPrice(...num1){ //... is called both rest and spread operator
+//     return num1
+// }
+
+// console.log(calculateCartPrice(200, 300, 400));
+
+function calculateCartPrice(val1, val2, ...num1){ //... is called both rest and spread operator
+    return num1
+}
+//output = [400,2000] val1 = 200 and val2 = 300
+// console.log(calculateCartPrice(200, 300, 400, 2000));
+
+const newObject = {
+    username: "aman",
+    price: 299
+}
+
+function salesPrice (anyObject){
+    console.log(`the coustomer ${anyObject.username} ordered an item worth ${anyObject.price}` );//we have to use function parameter inside string interpolation
+}
+
+//  salesPrice(newObject)
+// salesPrice({
+//     username: "sam",
+//     price: "399"
+// })
+
+const newArr = [200, 300, 400, 700]
+
+function retuSecondArray(getArray) {
+    return getArray[2]
+}
+// console.log(retuSecondArray(newArr));
+// console.log(retuSecondArray([200,34,435,546,2]));
+//scope- function,global instance
