31979a6bf680beac896fc4f063d1f413a4e9b0a4
Aman Singh <94058048+INFKIRITO@users.noreply.github.com>
2024-03-24 09:05:11 +0000

    message

---
+++ 05_loops/02while.js
@@ -0,0 +1,26 @@
+//while
+let index = 0 // index is written outside
+while (index <= 10) {
+   // console.log(`value of index ${index}`);
+    index = index + 2
+}
+
+let myArr = ['hulk', 'superman', 'goku', 'saitama']
+let arr = 0
+while(arr < myArr.length){
+   // console.log(`value is ${myArr[arr]}`);
+    arr = arr + 1
+}
+
+// Do While - condition is checked at the end of the loop
+// let score = 1 
+// do {
+//     console.log(`score is ${score}`);
+//     score++  
+// } while (score <= 10);
+let score = 11
+do {
+    console.log(`score is ${score}`);
+    score++  
+} while (score <= 10);
+
